0f8cbee4d4d7b0ffc2ea8a426dc7060014f89c98
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-04 15:34:05 +0100

    Added title

--- Drivers/PostPartDriver.cs
+++ Drivers/PostPartDriver.cs
@@ -1,12 +1,16 @@
 ﻿using JetBrains.Annotations;
 using NGM.Forum.Models;
 using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Routable.Models;
+using Orchard.ContentManagement;
 
 namespace NGM.Forum.Drivers {
     [UsedImplicitly]
     public class PostPartDriver : ContentPartDriver<PostPart> {
         protected override DriverResult Display(PostPart postPart, string displayType, dynamic shapeHelper) {
             return Combined(
+                ContentShape("Parts_Posts_Post_Title",
+                             () => shapeHelper.Parts_Posts_Post_Title(RoutePart: postPart.ThreadPart.As<RoutePart>())),
                 ContentShape("Parts_Posts_Post_Manage",
                              () => shapeHelper.Parts_Posts_Post_Manage(ContentPart: postPart))
                 );
