2ee650d02a5e408602e86ab2d2b97623e8302708
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-10 20:38:32 +0100

    added dynamiczoneextensions

--- Controllers/DynamicZoneExtensions.cs
+++ Controllers/DynamicZoneExtensions.cs
@@ -5,7 +5,7 @@ namespace NGM.Forum.Controllers {
         public static void RemoveItemFrom(dynamic zone, string itemToDelete) {
             var itemsToDelete = new List<object>();
 
-            foreach (var item in zone.items) {
+            foreach (var item in zone.Items) {
                 if (item.Metadata.Type == itemToDelete)
                     itemsToDelete.Add(item);
             }
