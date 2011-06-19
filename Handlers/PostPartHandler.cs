eaf84935145c1cc937e6fcbaeef68e1635173936
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-19 22:10:44 +0100

    fixing the post count calculation on forumPart

--- Handlers/PostPartHandler.cs
+++ Handlers/PostPartHandler.cs
@@ -68,7 +68,12 @@ namespace NGM.Forum.Handlers {
                                       _forumService.Get(commonPart.Record.Container.Id, VersionOptions.Published).As<ForumPart>();
 
                 forumPart.ContentItem.ContentManager.Flush();
-                forumPart.PostCount = _threadService.Get(forumPart, VersionOptions.Published).Count();
+
+                forumPart.PostCount = _threadService
+                    .Get(forumPart, VersionOptions.Published)
+                    .Sum(publishedThreadPart => _postService
+                        .Get(publishedThreadPart, VersionOptions.Published)
+                        .Count());
             }
         }
     }
