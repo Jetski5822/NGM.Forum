5144b52951a05dcc064408ec9021e5ebabc1fdd9
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-19 14:47:12 +0100

    Commenting out permissions.

--- Permissions.cs
+++ Permissions.cs
@@ -7,18 +7,18 @@ namespace NGM.Forum {
         public static readonly Permission ManageForums = new Permission { Description = "Manage forums", Name = "ManageForums" };
 
         public static readonly Permission AddThread = new Permission { Description = "Add a thread", Name = "AddThread" };
-        public static readonly Permission EditThread = new Permission { Description = "Edit any thread", Name = "EditThread", ImpliedBy = new[] { AddThread } };
-        public static readonly Permission DeleteThread = new Permission { Description = "Delete any thread", Name = "DeleteThread", ImpliedBy = new [] { AddThread }};
-        public static readonly Permission CloseThread = new Permission { Description = "Can close a thread", Name = "CloseThread", ImpliedBy = new[] { ManageForums } };
-        public static readonly Permission OpenThread = new Permission { Description = "Can open a thread", Name = "OpenThread", ImpliedBy = new[] { ManageForums } };
-        public static readonly Permission ManageStickyThread = new Permission { Description = "Can create a thread that is sticky", Name = "CreateStickyThread", ImpliedBy = new[] { ManageForums } };
+        public static readonly Permission EditThread = new Permission { Description = "Edit any thread", Name = "EditThread" };
+        public static readonly Permission DeleteThread = new Permission { Description = "Delete any thread", Name = "DeleteThread" };
+        public static readonly Permission CloseThread = new Permission { Description = "Can close a thread", Name = "CloseThread" };
+        public static readonly Permission OpenThread = new Permission { Description = "Can open a thread", Name = "OpenThread" };
+        public static readonly Permission ManageStickyThread = new Permission { Description = "Can create a thread that is sticky", Name = "ManageStickyThread" };
 
         public static readonly Permission AddPost = new Permission { Description = "Add a post", Name = "AddPost" };
-        public static readonly Permission EditPost = new Permission { Description = "Edit a post", Name = "EditPost", ImpliedBy = new[] { AddPost } };
-        public static readonly Permission DeletePost = new Permission { Description = "Delete a post", Name = "DeletePost", ImpliedBy = new[] { AddPost } };
+        public static readonly Permission EditPost = new Permission { Description = "Edit a post", Name = "EditPost"  };
+        public static readonly Permission DeletePost = new Permission { Description = "Delete a post", Name = "DeletePost" };
 
-        public static readonly Permission MetaListForum = new Permission { ImpliedBy = new[] { AddThread, EditThread, DeleteThread, CloseThread, OpenThread, ManageStickyThread } };
-        public static readonly Permission MetaListThread = new Permission { ImpliedBy = new[] { AddPost, EditPost, DeletePost } };
+        //public static readonly Permission MetaListForum = new Permission { ImpliedBy = new[] { AddThread, EditThread, DeleteThread, CloseThread, OpenThread, ManageStickyThread } };
+        //public static readonly Permission MetaListThread = new Permission { ImpliedBy = new[] { AddPost, EditPost, DeletePost } };
 
         public virtual Feature Feature { get; set; }
 
@@ -38,35 +38,36 @@ namespace NGM.Forum {
         }
 
         public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
+
             return new[] {
                 new PermissionStereotype {
                     Name = "Administrator",
-                    Permissions = new[] {ManageForums, MetaListForum, MetaListThread}
+                    Permissions = new[] {ManageForums}
                 },
-                new PermissionStereotype {
-                    Name = "Anonymous",
-                    Permissions = new[] {MetaListForum, MetaListThread}
-                },
-                new PermissionStereotype {
-                    Name = "Authenticated",
-                    Permissions = new[] {MetaListForum, MetaListThread}
-                },
-                new PermissionStereotype {
-                    Name = "Editor",
-                    Permissions = new[] {MetaListForum, MetaListThread}
-                },
-                new PermissionStereotype {
-                    Name = "Moderator",
-                    Permissions = new[] {MetaListForum, MetaListThread}
-                },
-                new PermissionStereotype {
-                    Name = "Author",
-                    Permissions = new[] {MetaListForum, MetaListThread}
-                },
-                new PermissionStereotype {
-                    Name = "Contributor",
-                    Permissions = new[] {MetaListForum, MetaListThread}
-                },
+                //new PermissionStereotype {
+                //    Name = "Anonymous",
+                //    Permissions = new[] {MetaListForum, MetaListThread}
+                //},
+                //new PermissionStereotype {
+                //    Name = "Authenticated",
+                //    Permissions = new[] {MetaListForum, MetaListThread}
+                //},
+                //new PermissionStereotype {
+                //    Name = "Editor",
+                //    Permissions = new[] {MetaListForum, MetaListThread}
+                //},
+                //new PermissionStereotype {
+                //    Name = "Moderator",
+                //    Permissions = new[] {MetaListForum, MetaListThread}
+                //},
+                //new PermissionStereotype {
+                //    Name = "Author",
+                //    Permissions = new[] {MetaListForum, MetaListThread}
+                //},
+                //new PermissionStereotype {
+                //    Name = "Contributor",
+                //    Permissions = new[] {MetaListForum, MetaListThread}
+                //},
             };
         }
 
