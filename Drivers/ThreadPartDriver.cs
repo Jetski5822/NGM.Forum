84ae6415ea1800e99ebbb343a625dc733a703057
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-26 10:52:25 +0100

    Sticky now works.. against Threads.

--- Drivers/ThreadPartDriver.cs
+++ Drivers/ThreadPartDriver.cs
@@ -2,11 +2,23 @@
 using NGM.Forum.Models;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.Security;
 
 namespace NGM.Forum.Drivers {
     [UsedImplicitly]
     public class ThreadPartDriver : ContentPartDriver<ThreadPart> {
-        private const string TemplateName = "Parts.Routable.ThreadPart";
+        private const string RoutableTemplateName = "Parts.Routable.ThreadPart";
+        private const string StatusTemplateName = "Parts.Status.ThreadPart";
+
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IAuthorizationService _authorizationService;
+
+        public ThreadPartDriver(
+            IAuthenticationService authenticationService,
+            IAuthorizationService authorizationService) {
+            _authenticationService = authenticationService;
+            _authorizationService = authorizationService;
+        }
 
         protected override string Prefix {
             get { return "ThreadPart"; }
@@ -30,9 +42,17 @@ namespace NGM.Forum.Drivers {
                 );
         }
 
-        protected override DriverResult Editor(ThreadPart part, dynamic shapeHelper) {
-            return ContentShape("Parts_Routable_Thread_Edit",
-                () => shapeHelper.EditorTemplate(TemplateName: TemplateName, Model: part, Prefix: Prefix));
+        protected override DriverResult Editor(ThreadPart threadPart, dynamic shapeHelper) {
+            if (!_authorizationService.TryCheckAccess(Permissions.ManageStickyThread, _authenticationService.GetAuthenticatedUser(), threadPart))
+                return ContentShape("Parts_Routable_Thread_Edit",
+                    () => shapeHelper.EditorTemplate(TemplateName: RoutableTemplateName, Model: threadPart, Prefix: Prefix));
+
+            return
+                Combined(
+                    ContentShape("Parts_Status_Thread_Edit",
+                                 () => shapeHelper.EditorTemplate(TemplateName: StatusTemplateName, Model: threadPart, Prefix: Prefix)),
+                    ContentShape("Parts_Routable_Thread_Edit",
+                                 () => shapeHelper.EditorTemplate(TemplateName: RoutableTemplateName, Model: threadPart, Prefix: Prefix)));
         }
 
         protected override DriverResult Editor(ThreadPart part, IUpdateModel updater, dynamic shapeHelper) {
