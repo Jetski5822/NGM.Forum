6cadeb7f86983027daafe7345bf8552f38f878b6
Nicholas Mayne <Jetski5822@gmail.com>
2011-06-26 12:25:03 +0100

    Added Is Locked status to forums.

--- Drivers/ForumPartDriver.cs
+++ Drivers/ForumPartDriver.cs
@@ -7,6 +7,8 @@ namespace NGM.Forum.Drivers {
     public class ForumPartDriver : ContentPartDriver<ForumPart> {
         protected override DriverResult Display(ForumPart forumPart, string displayType, dynamic shapeHelper) {
             return Combined(
+                ContentShape("Parts_Forums_Forum_Status",
+                             () => shapeHelper.Parts_Forums_Forum_Status(ContentPart: forumPart)),
                 ContentShape("Parts_Forums_Forum_Manage",
                              () => shapeHelper.Parts_Forums_Forum_Manage(ContentPart: forumPart)),
                 ContentShape("Parts_Forums_Forum_ThreadCount",
