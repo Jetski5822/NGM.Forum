a17dd8f037615291ab2c3489bdb0390284e3fcdf
Nicholas Mayne <Jetski5822@gmail.com>
2011-07-23 11:04:59 +0100

    Added style sheet

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -3,7 +3,9 @@ using Orchard.UI.Resources;
 namespace NGM.Forum {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
-            builder.Add().DefineStyle("ForumsAdmin").SetUrl("orchard-forum-admin.css");
+            var resourceManifest = builder.Add();
+            resourceManifest.DefineStyle("ForumsAdmin").SetUrl("orchard-forum-admin.css");
+            resourceManifest.DefineStyle("Forums").SetUrl("forum.css");
         }
     }
 }
