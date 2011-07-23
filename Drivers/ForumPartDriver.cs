28e0b6a2363c948ef6efc09da6431e0af7c52abc
Nicholas Mayne <Jetski5822@gmail.com>
2011-07-23 11:24:25 +0100

    All modifications to parts must be done in the admin area. I broke this rule, slapped my self, and now im back on track

--- Drivers/ForumPartDriver.cs
+++ Drivers/ForumPartDriver.cs
@@ -1,10 +1,13 @@
 ﻿using JetBrains.Annotations;
 using NGM.Forum.Models;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
 
 namespace NGM.Forum.Drivers {
     [UsedImplicitly]
     public class ForumPartDriver : ContentPartDriver<ForumPart> {
+        private const string OpenClosedTemplateName = "Parts.OpenClosed.ForumPart";
+
         protected override DriverResult Display(ForumPart forumPart, string displayType, dynamic shapeHelper) {
             var postCount = forumPart.PostCount >= 1 ? forumPart.PostCount - 1 : forumPart.PostCount;
 
@@ -18,6 +21,18 @@ namespace NGM.Forum.Drivers {
                 ContentShape("Parts_Forums_Forum_PostCount",
                              () => shapeHelper.Parts_Forums_Forum_PostCount(ContentPart: forumPart, PostCount: postCount))
                 );
+        }
+
+        protected override DriverResult Editor(ForumPart part, dynamic shapeHelper) {
+            return
+                Combined(
+                    ContentShape("Parts_OpenClosed_Forum_Edit",
+                                 () => shapeHelper.EditorTemplate(TemplateName: OpenClosedTemplateName, Model: part, Prefix: Prefix)));
+        }
+
+        protected override DriverResult Editor(ForumPart part, IUpdateModel updater, dynamic shapeHelper) {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
         }
     }
 }
