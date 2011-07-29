441c3758de29061775ee924c725c6be60d3f4f78
Nicholas Mayne <Jetski5822@gmail.com>
2011-07-29 22:38:44 +0100

    Stopping ability of replying if thread is closed.

--- Drivers/PostPartDriver.cs
+++ Drivers/PostPartDriver.cs
@@ -14,13 +14,13 @@ namespace NGM.Forum.Drivers {
         protected override DriverResult Display(PostPart postPart, string displayType, dynamic shapeHelper) {
             if (postPart.IsParentThread())
                 return Combined(ContentShape("Parts_Posts_Post_Manage",
-                                             () => shapeHelper.Parts_Posts_Post_Manage(ContentPart: postPart)));
+                                             () => shapeHelper.Parts_Posts_Post_Manage(ContentPart: postPart, IsClosed: postPart.ThreadPart.IsClosed)));
 
                 return Combined(
                     ContentShape("Parts_Posts_Post_Title",
                                  () => shapeHelper.Parts_Posts_Post_Title(RoutePart: postPart.ThreadPart.As<RoutePart>())),
                     ContentShape("Parts_Posts_Post_Manage",
-                                 () => shapeHelper.Parts_Posts_Post_Manage(ContentPart: postPart))
+                                 () => shapeHelper.Parts_Posts_Post_Manage(ContentPart: postPart, IsClosed: postPart.ThreadPart.IsClosed))
                     );
         }
     }
--- Extensions/UrlHelperExtensions.cs
+++ Extensions/UrlHelperExtensions.cs
@@ -26,14 +26,18 @@ namespace NGM.Forum.Extensions {
             return urlHelper.Action("Create", "Thread", new { forumId = forumPart.Id, area = Constants.LocalArea });
         }
 
-        public static string ReplyPost(this UrlHelper urlHelper, IContent content) {
+        public static string ReplyPost(this UrlHelper urlHelper, PostPart postPart) {
+            return ReplyContent(urlHelper, postPart);
+        }
+
+        public static string ReplyContent(this UrlHelper urlHelper, IContent content) {
             return urlHelper.Action("Create", "Post", new { contentId = content.Id, area = Constants.LocalArea });
         }
 
         public static string ReplyThread(this UrlHelper urlHelper, ThreadPart threadPart) {
-            return ReplyPost(urlHelper, threadPart);
+            return ReplyContent(urlHelper, threadPart);
         }
-
+        
         public static string OpenThread(this UrlHelper urlHelper, ThreadPart threadPart) {
             return urlHelper.Action("Open", "Thread", new { threadId = threadPart.Id, area = Constants.LocalArea });
         }
