5d7c98260eb9ba618b5e02f9e44437cd31d85a79
Nicholas Mayne <Jetski5822@gmail.com>
2011-09-07 19:06:31 +0100

    commented out quick edit. will work on this later. a little css change

---
+++ Scripts/quick-edit.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+    $(".create").live("click",
+        function (e) {
+            e.preventDefault();
+            var currentUrl = $(this).attr('href');
+
+            $.ajax({
+                url: currentUrl,
+                success: function (data) {
+                    var str = $(data).find(".zone-content")
+                    $(".zone-content").append(str);
+                }
+            });
+        });
+})(jQuery);
