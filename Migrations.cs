3e7f85c40d1bc873532035dcee3ab83ad9e686df
Nicholas Mayne <Jetski5822@gmail.com>
2012-10-13 12:05:00 +0100

    updating migrations and module dependencies

--- Migrations.cs
+++ Migrations.cs
@@ -47,50 +47,51 @@ namespace NGM.Forum {
             ContentDefinitionManager.AlterPartDefinition("ThreadPart", builder => builder
                 .WithField("Categories", cfg => cfg
                     .OfType("TaxonomyField")
-                    .WithSetting("TaxonomyFieldSettings.AllowCustomTerms", "true")
+                    .WithSetting("TaxonomyFieldSettings.AllowCustomTerms", "false")
+                    .WithSetting("TaxonomyFieldSettings.SingleChoice", "true")
+                    .WithSetting("TaxonomyFieldSettings.Required", "true")
                     .WithSetting("TaxonomyFieldSettings.Taxonomy", categoryTaxonomyPart.Name))
                 .WithField("Tags", cfg => cfg
                     .OfType("TaxonomyField")
                     .WithSetting("TaxonomyFieldSettings.AllowCustomTerms", "true")
-                    .WithSetting("TaxonomyFieldSettings.Taxonomy", tagsTaxonomyPart.Name)));
+                    .WithSetting("TaxonomyFieldSettings.Autocomplete", "true")
+                    .WithSetting("TaxonomyFieldSettings.Required", "true")
+                    .WithSetting("TaxonomyFieldSettings.Taxonomy", tagsTaxonomyPart.Name))
+            );
 
-            ContentDefinitionManager.AlterTypeDefinition("Forum",
-                cfg => cfg
-                    .WithPart("ForumPart")
-                    .WithPart("CommonPart")
-                    .WithPart("TitlePart")
-                    .WithPart("AutoroutePart", builder => builder
-                        .WithSetting("AutorouteSettings.AllowCustomPattern", "true")
-                        .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
-                        .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Title', Pattern: '{Content.Slug}', Description: 'my-forum'}]")
-                        .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
-                    .WithPart("MenuPart")
-                );
+            ContentDefinitionManager.AlterTypeDefinition("Forum", cfg => cfg
+                .WithPart("ForumPart")
+                .WithPart("CommonPart")
+                .WithPart("TitlePart")
+                .WithPart("AutoroutePart", builder => builder
+                    .WithSetting("AutorouteSettings.AllowCustomPattern", "true")
+                    .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
+                    .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Title', Pattern: '{Content.Slug}', Description: 'my-forum'}]")
+                    .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
+                .WithPart("MenuPart")
+            );
 
-            ContentDefinitionManager.AlterTypeDefinition("Thread",
-                cfg => cfg
-                    .WithPart("ThreadPart")
-                    .WithPart("CommonPart", builder => builder
-                        .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false"))
-                    .WithPart("TitlePart")
-                    .WithPart("AutoroutePart", builder => builder
-                        .WithSetting("AutorouteSettings.AllowCustomPattern", "false")
-                        .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
-                        .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Forum and Title', Pattern: '{Content.Container.Path}/{Content.Slug}', Description: 'my-forum/my-thread'}]")
-                        .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
-                );
+            ContentDefinitionManager.AlterTypeDefinition("Thread", cfg => cfg
+                .WithPart("ThreadPart")
+                .WithPart("CommonPart", builder => builder
+                    .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false"))
+                .WithPart("TitlePart")
+                .WithPart("AutoroutePart", builder => builder
+                    .WithSetting("AutorouteSettings.AllowCustomPattern", "false")
+                    .WithSetting("AutorouteSettings.AutomaticAdjustmentOnEdit", "false")
+                    .WithSetting("AutorouteSettings.PatternDefinitions", "[{Name:'Forum and Title', Pattern: '{Content.Container.Path}/{Content.Slug}', Description: 'my-forum/my-thread'}]")
+                    .WithSetting("AutorouteSettings.DefaultPatternIndex", "0"))
+            );
 
-            ContentDefinitionManager.AlterTypeDefinition("Post",
-                cfg => cfg
-                    .WithPart("PostPart")
-                    .WithPart("CommonPart", builder => builder
-                        .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false"))
-                );
+            ContentDefinitionManager.AlterTypeDefinition("Post", cfg => cfg
+                .WithPart("PostPart")
+                .WithPart("CommonPart", builder => builder
+                    .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false"))
+            );
 
-            ContentDefinitionManager.AlterTypeDefinition("User",
-                cfg => cfg
-                    .WithPart("UserForumPart")
-                );
+            ContentDefinitionManager.AlterTypeDefinition("User", cfg => cfg
+                .WithPart("UserForumPart")
+            );
 
             return 1;
         }
