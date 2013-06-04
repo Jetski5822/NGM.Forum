d86ef3bb4dbbde0a0578cd2c48cc838660e2a971
Nicholas Mayne <Jetski5822@gmail.com>
2013-06-04 17:15:05 +0100

    Removing unused CSS

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -5,10 +5,8 @@ namespace NGM.Forum {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var resourceManifest = builder.Add();
             resourceManifest.DefineStyle("ForumsAdmin").SetUrl("ngm-forum-admin.css");
-            resourceManifest.DefineStyle("Forums").SetUrl("forum.css");
 
             resourceManifest.DefineStyle("ThreadsAdmin").SetUrl("ngm-thread-admin.css");
-            resourceManifest.DefineStyle("Threads").SetUrl("thread.css");
         }
     }
 }
